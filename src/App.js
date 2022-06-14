acbbe8f040071575ecfdc093b5395a265f6c127f
meghacsnair <megha.cnair@publicissapient.com>
2022-06-14 11:14:09 +0530

    Added app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      name:"Megha"
+    }
+    this.handleNamechange = this.handleNamechange.bind(this)
+  }
+  handleNamechange = ()=>{
+    this.setState({name:"Surya"})
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hi {this.state.name}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={this.handleNamechange}>Change Name</button>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
